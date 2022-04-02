fb0465e2151e3f8d1edc7cf5c266fc470dc6eee5
Mikhail Ivanovskii <81823117+Muis903@users.noreply.github.com>
2022-04-02 20:29:32 +0200

    forEach_map_filter

---
+++ week_1.js
@@ -0,0 +1,27 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with buisnesspartner',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false
+    }
+];
+
+// forEach, map, filter
+
+const todoCompleted = todos.filter(function(todo) {
+    return todo.isCompleted == true;
+}).map(function(todo){
+    return todo.text;
+});
+
+console.log(todoCompleted);
